5da2d8b0e04584557c9a1e8b653c82441c35d699
anirudt <anirudt@gmail.com>
2016-03-19 12:18:10 +0530

    adds proxy settings

--- scripter.sh
+++ scripter.sh
@@ -2,9 +2,14 @@
 
 # Adds Proxy settings for the Pi
 add_proxy_settings() {
-  sudo :> /etc/apt/apt.conf.d/10proxy
   my_http_proxy="http://10.3.100.207:8080/"
   my_https_proxy="https://10.3.100.207:8080/"
+  apt_http_line="Acquire::http::proxy $my_http_proxy;"
+  apt_https_line="Acquire::https::proxy $my_https_proxy;"
+  sudo echo "$apt_http_line\n$apt_https_line" > /etc/apt/apt.conf.d/10proxy;
+  bash_http_proxy="export http_proxy=$my_http_proxy"
+  bash_https_proxy="export https_proxy=$my_https_proxy"
+  sudo echo "$bash_http_proxy\n$bash_https_proxy" >> ~/.bashrc
 }
 
 # Git checker
@@ -21,3 +26,4 @@ if git_exists; then
 else
   sudo apt-get install -y git
 fi
+add_proxy_settings
