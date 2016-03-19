9e42e6805e9026bc151508b4a068138ba3298ee6
anirudt <anirudt@gmail.com>
2016-03-19 08:54:53 +0530

    started proxy settings adder

--- scripter.sh
+++ scripter.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Adds Proxy settings for the Pi
+add_proxy_settings() {
+  sudo :> /etc/apt/apt.conf.d/10proxy
+  my_http_proxy="http://10.3.100.207:8080/"
+  my_https_proxy="https://10.3.100.207:8080/"
+}
+
 # Git checker
 git_exists() {
   type git &> /dev/null;
@@ -10,5 +17,7 @@ git_exists() {
 #                     Main Module                                 #
 ###################################################################
 if git_exists; then
-  echo "Hello"
+  echo "Git already installed on system"
+else
+  sudo apt-get install -y git
 fi
