8beaf985079c5e584d32ee023582a339333b0aeb
anirudt <anirudt@gmail.com>
2016-03-21 12:00:53 +0530

    added opencv dependencies, cloning

--- scripter.sh
+++ scripter.sh
@@ -29,6 +29,28 @@ vim_exists() {
   type vim &> /dev/null;
 }
 
+prepare_for_cv() {
+  echo "Installing OpenCV dependencies .. .."
+  sudo apt-get install -y build-essential;
+  sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+  sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+  echo "Installed OpenCV dependencies."
+}
+
+install_cv() {
+  git clone https://github.com/Itseez/opencv.git
+  echo "OpenCV repository successfully cloned."
+  echo "Make the release directory? Press (y/n)"
+  read MAKE_RELEASE
+  if [[ $MAKE_RELEASE == y ]]; then
+    mkdir opencv/release; cd opencv/release;
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+    echo "Please proceed to build OpenCV."
+  else
+    echo "Configure using cmake separately."
+  fi
+}
+
 display() {
   echo "*Welcome to the Rapberry Pi install portal*";
   echo "Press the sequence of numbers for the required tasks:";
@@ -36,8 +58,10 @@ display() {
   echo "b-> Install Git";
   echo "c-> Install Vim";
   echo "d-> Clone custom Git repository [BTP]";
-  echo "e-> Install CMake, OpenCV";
   echo "f-> Install PiPan package"
+  echo "g-> Install OpenCV Dependencies"
+  echo "i-> Clone OpenCV repository & make release dir"
+  echo "q-> Install CMake";
   echo "Usage:";
   echo "$ ./scripter.sh 123";
   echo "for running 1, 2, and 3.";
@@ -78,13 +102,6 @@ main() {
       git clone https://anirudt_bit@bitbucket.org/anirudt_bit/btp.git;
     fi
   fi
-  if [[ "$options" == *e* ]]; then
-    if cmake_exists; then
-      echo "CMake is already installed on system"
-    else
-      sudo apt-get install -y cmake
-    fi
-  fi
   if [[ "$options" == *f* ]]; then
     if [ -d btp/pi-pan ]; then
       cp -r btp/pi-pan/ pi-pan/
@@ -96,7 +113,20 @@ main() {
             please install BTP package first"
     fi
   fi
-  if [[ "$options" == *h* ]]; then
+  if [[ "$options" == *g* ]]; then
+    prepare_for_cv
+  fi
+  if [[ "$options" == *i* ]]; then
+    install_cv
+  fi
+  if [[ "$options" == *q* ]]; then
+    if cmake_exists; then
+      echo "CMake is already installed on system"
+    else
+      sudo apt-get install -y cmake
+    fi
+  fi
+  if [[ "$options" == *help* ]]; then
     display
   fi
 }
